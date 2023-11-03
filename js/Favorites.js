8e882cbb5f08080c3117c69ae1d9ac42f9bd1e93
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2023-11-03 17:44:09 +0100

    js delete

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,7 +1,28 @@
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
+        this.load()
+        this.tbody = this.root.querySelector("table tbody")
     }
+
+    load() {
+        this.entries = [
+            {
+                login: 'nandaospain',
+                name: 'nandaospain',
+                public_repos: '35',
+                followers: '18'
+            }
+        ]
+        
+    }
+
+    delete(user) {
+        const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
+
+        this.entries = filteredEntries
+        this.update()
+    }   
 }
 
 export class FavoritesView extends Favorites {
@@ -13,14 +34,33 @@ export class FavoritesView extends Favorites {
     update() {
         this.removeAllTr()
 
-        [
-            {
-                login: '',
-                name: '',
-                public_repos: '',
-                followers: ''
+        this.entries.forEach( user => {
+            const row = this.createRow()
+
+            row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+
+            row.querySelector('.user img').alt = `Imagem de ${user.name}`
+
+            row.querySelector('.user p').textContent = user.name
+
+            row.querySelector('.user span').textContent = user.login
+
+            row.querySelector('.repositories').textContent = user.public_repos
+
+            row.querySelector('.followers').textContent = user.followers
+
+            row.querySelector('.remove').onclick = () => {
+                const isOk = confirm('Tem certeza que deseja deletar esse usuário?')
+                if(isOk) {
+                    this.delete(user)
+                }
             }
-        ]
+
+
+
+            this.tbody.append(row)
+        })
+        
     }
     
     createRow() {
@@ -43,10 +83,8 @@ export class FavoritesView extends Favorites {
         return tr
     }   
 
-    removeAllTr() {
-        const tbody = this.root.querySelector("table tbody")
-    
-        tbody.querySelectorAll('tr').forEach((tr) => {
+    removeAllTr() {    
+        this.tbody.querySelectorAll('tr').forEach((tr) => {
             tr.remove()
         })        
     }
