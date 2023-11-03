fa07122fb657ce2767b20d4e7ccc7f45c3c31017
Fernando Rodrigues <117524258+NandaoSpain@users.noreply.github.com>
2023-11-03 17:03:40 +0100

    js create row

--- js/Favorites.js
+++ js/Favorites.js
@@ -11,8 +11,43 @@ export class FavoritesView extends Favorites {
     }
 
     update() {
-        const tbody = this.root.querySelector("table tbody")
+        this.removeAllTr()
 
-        console.log(tbody.querySelectorAll('tr'))
+        [
+            {
+                login: '',
+                name: '',
+                public_repos: '',
+                followers: ''
+            }
+        ]
+    }
+    
+    createRow() {
+        const tr = document.createElement('tr')
+        
+        tr.innerHTML = `
+            <td class="user">
+                <img src="https://github.com/nandaospain.png" alt="">
+                <a href="https://github.com/nandaospain" target="_blank">
+                    <p>Fernando Rodrigues</p>
+                    <span>nandaospain</span>
+                </a>
+            </td>
+            <td class="repositories">35</td>
+            <td class="followers">18</td>
+            <td>
+                <button class="remove">&times;</button>
+            </td>    
+            `   
+        return tr
+    }   
+
+    removeAllTr() {
+        const tbody = this.root.querySelector("table tbody")
+    
+        tbody.querySelectorAll('tr').forEach((tr) => {
+            tr.remove()
+        })        
     }
 }
